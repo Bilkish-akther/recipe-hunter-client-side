fb7e1e4ad4129f1df8a89b4670e8c82f8637feaf
Bilkish-akther <121663376+Bilkish-akther@users.noreply.github.com>
2023-05-05 23:44:39 +0600

    Add files via upload
    upda code

---
+++ DeliciousFood.jsx
@@ -0,0 +1,48 @@
+/*eslint-disable no-unused-vars */
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
+const DeliciousFood = () => {
+  return (
+    <div className="mt-4 text-center">
+    
+    <h1>More New Recipe</h1>
+    <h4>A large selection of dishes for cocking</h4>
+    <div>
+      <Container>
+      <Row xs={1} md={4} className=" ">
+            {Array.from({ length: 3 }).map((_, idx) => (
+              <Col key={idx}>
+                <Card>
+
+                  <Card.Img variant="top" src="https://i.ibb.co/yNkcBq4/images.jpg" />
+
+                  <Card.Title>Brackfast</Card.Title>
+                </Card>
+                <Card>
+                  <Card.Img variant="top" src="https://i.ibb.co/QpMhjK1/download-1.jpg" />
+
+                  <Card.Title>Launch</Card.Title>
+                </Card>
+
+                <Card>
+                  <Card.Img variant="top" src="https://i.ibb.co/t8jvLxy/download-2.jpg" />
+
+                  <Card.Title>Dinner</Card.Title>
+                </Card>
+                <Card>
+                  <Card.Img variant="top" src="https://i.ibb.co/37h5KW6/category2.jpg" />
+
+                  <Card.Title>Drinks</Card.Title>
+                </Card>
+
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default DeliciousFood;
